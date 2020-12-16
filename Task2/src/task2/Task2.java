a64abaef6ec48fb5f2668c4821251a905880c3f9
SohailMG <72792684+SohailMG@users.noreply.github.com>
2020-12-16 15:57:01 +0000

    added a filltable function to read file data and adds it to a 2d array

--- Task2/src/task2/Task2.java
+++ Task2/src/task2/Task2.java
@@ -2,72 +2,80 @@ package task2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * A Memory Game where user is given a grid of hidden words.
- * Main goal is to find the matching Pairs.
- * 
- *@author Sohail Gsais
- * 
+ * A Memory Game where user is given a grid of hidden words. Main goal is to
+ * find the matching Pairs.
+ *
+ * @author Sohail Gsais
+ *
  */
 public class Task2 {
 
     public static void main(String[] args) {
-        
-        
-        String[][] GameGrid = 
-               {{"1  ","  ******   ", "   ******    ", "   ******    ", "   ******    "},
-                {"2  ","  ******   ", "   ******    ", "   ******    ", "   ******    "},
-                {"3  ","  ******   ", "   ******    ", "   ******    ", "   ******    "},
-                {"4  ","  ******   ", "   ******    ", "   ******    ", "   ******    "}};
-        String[][] GameWords = 
-               {{"1  ","  ******   ", "   ******    ", "   ******    ", "   ******    "},
-                {"2  ","  ******   ", "   ******    ", "   ******    ", "   ******    "},
-                {"3  ","  ******   ", "   ******    ", "   ******    ", "   ******    "},
-                {"4  ","  ******   ", "   ******    ", "   ******    ", "   ******    "}};
-        
+
+        String[][] GameGrid
+                = {{"1  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "},
+                {"2  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "},
+                {"3  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "},
+                {"4  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "}};
+        String[][] GameWords
+                = {{"1  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "},
+                {"2  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "},
+                {"3  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "},
+                {"4  ", "  ******   ", "   ******    ", "   ******    ", "   ******    "}};
+
         showGameGrid(GameGrid);
-        
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Choose Position > ");
         int pos = scan.nextInt();
-        
-        showWord(GameGrid,pos);
+
+        showWord(GameGrid, pos);
         showGameGrid(GameGrid);
-        FILEdata();
-        
-        
-        
+        fillTable();
+
+        String word = null;
+
+//        for (int i = 0; i < 7; i++) {
+//            
+//         word = GetfileData (i);
+//         
+//        }
     }
-    
+
     /**
      * displays the Game Grid
+     *
      * @param GameGrid
      */
-    public static void showGameGrid(String[][] GameGrid){
-        
-        System.out.println("" +  "        *" + "        *" + "        *" + "       *");
+    public static void showGameGrid(String[][] GameGrid) {
+
+        System.out.println("" + "        *" + "        *" + "        *" + "       *");
         System.out.println("  ____________________________________");
-        
-        for(String[]row : GameGrid){
-            for (String s : row){
+
+        for (String[] row : GameGrid) {
+            for (String s : row) {
                 System.out.print(s);
             }
-            System.out.println( );
+            System.out.println();
         }
-    
-    
-    
+
     }
+
     /**
      * shows word in selected position
+     *
      * @param GameGrid
      * @param pos
      */
-    public static void showWord(String[][] GameGrid,int pos){
-        
-         switch(pos){
+    public static void showWord(String[][] GameGrid, int pos) {
+
+        switch (pos) {
             case 1:
                 GameGrid[0][2] = "Sohail";
                 break;
@@ -79,43 +87,69 @@ public class Task2 {
                 break;
             default:
                 System.out.println("Position Out of range");
-        
+
         }
-        
-        
+
     }
-    
-    
-    
-    public static void fillGrid(String[][] GameWords ){
-            int [][] arr = new int [5][5];
+
+//    public static String GetfileData (int item){
+//        ArrayList<String> words = new ArrayList<>();
+//        
+//      
+//            File myObj = new File("small.txt");
+//        try {
+//           Scanner myReader = new Scanner(myObj);
+//                while (myReader.hasNextLine()) {
+//                    String data = myReader.nextLine();
+//                    words.add(data);
+//                    
+//                    
+//                }
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("File not found");
+//        }
+//
+//        return words.get(item);
+//    
+//    }
+    public static void fillTable() {
+
+        File myObj = new File("small.txt");
+
+        int colms = 4;
+        int rows = 2;
+
+        String[][] table = new String[rows][colms];
         
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                arr[i][j] =  (int) (Math.random()* 11) ;
-            }
-        }
-        for(int k = 0; k < 3; k++){
-            for(int l = 0; l < 3; l++){
-                System.out.print(arr[k][l] + " ");
-            }
-            System.out.println();
-        }
-    }
-    
-    public static void FILEdata(){
+        ArrayList<String> ws = new ArrayList<>();
+       int item = 0;
         
         try {
-      File myObj = new File("easy.txt");
-            try (Scanner myReader = new Scanner(myObj)) {
-                while (myReader.hasNextLine()) {
-                    String data = myReader.nextLine();
-                    System.out.println(data);
-                }     }
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-    } 
+            Scanner scanF = new Scanner(myObj);
+            while (scanF.hasNextLine()) {
 
-    
+                 ws.add(scanF.nextLine());
+            }
+//            System.out.println(ws);
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < colms; j++) {
+                    table[i][j] = ws.get(item);
+                    item++;
+                }
+
+            }
+            for (String[] a : table) {
+                for (String i : a) {
+                    System.out.print(i + "\t");
+                }
+                System.out.println("\n");
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");;
+        }
+
     }
+
 }
