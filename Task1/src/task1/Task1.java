3df1c8b7eb6d8ddd2c49537b5d8355e8008f4b17
SohailMG <72792684+SohailMG@users.noreply.github.com>
2020-12-18 11:39:02 +0000

    fixed string out of bound error

--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -2,108 +2,89 @@ package task1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author sohailgsais
- */
 public class Task1 {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) {
 
-        // file to parse from 
-        File myObj = new File("datafile.txt");
+        String dataFile = System.getProperty("user.dir") + File.separator + "datafile.txt";
 
-        // file to write decripted FileContent to 
-        FileWriter myWriter = new FileWriter("dataout.txt");
+        char[] vowels = {' ', 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y'};
+        char[] consonants = {' ', 'b', 'B', 'c', 'C', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H',
+            'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'p', 'P',
+            'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'v', 'V', 'w', 'W', 'x', 'X', 'z', 'Z'};
 
-        // list  of Vowels
-        List<String> Vowels = new ArrayList<>();
-        Collections.addAll(Vowels, " ", "a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "y", "Y");
+        File f = new File(dataFile);
 
-        //list of consonants
-        List<String> Consonants = new ArrayList<>();
-        Collections.addAll(Consonants, " ", "b", "B", "c", "C", "d", "D", "f", "F", "g", "G",
-                "h", "H", "j", "J", "k", "K", "l", "L", "m", "M",
-                "n", "N", "p", "P", "q", "Q", "r", "R", "s", "S",
-                "t", "T", "v", "V", "w", "W", "x", "X", "z", "Z");
+        try {
+            Scanner fileReader = new Scanner(f);
 
-        // initialising string to hold file encrypted file FileContent
-        String FileContent = null;
+            while (fileReader.hasNextLine()) {
 
-        try (Scanner myReader = new Scanner(myObj)) {
+                String encryption = fileReader.nextLine();
+                String regex = "\\d+";
+                System.out.println("");
 
-            while (myReader.hasNextLine()) {
+                for (int i = 0; i < encryption.length(); i++) {
 
-                // storing file content 
-                FileContent = myReader.nextLine();
+                    char c = encryption.charAt(i);
 
-                System.out.println("\n");
-                myWriter.write("\n");
+                    if (c == 'C') {
+                        try {
+                            String Cnum = encryption.substring(i, i + 3);
 
-                /**
-                 * parsing through file content to check if char is a C or a V
-                 * if it's a C it will check if the next two items are digits if
-                 * it is a digit it will convert it to an integer
-                 */
-                for (int i = 0; i < FileContent.length() - 2; i++) {
+                            Pattern p = Pattern.compile(regex);
+                            Matcher m = p.matcher(Cnum);
 
-                    if (FileContent.charAt(i) == 'C') {
-                        if (Character.isDigit(FileContent.charAt(i + 2))) {
-                            String x = FileContent.substring(i + 1, i + 3);
-                            int num = Integer.parseInt(x);
-
-                            System.out.print(Consonants.get(num));
-                            myWriter.write(Consonants.get(num));
-
-                        } else {
-                            char c = FileContent.charAt(i + 1);
-                            int Cnum = Character.getNumericValue(c);
-                            System.out.print(Consonants.get(Cnum));
-                            myWriter.write(Consonants.get(Cnum));
+                            while (m.find()) {
+                                int num = Integer.parseInt(m.group());
+                                System.out.print(consonants[num]);
+                            }
+                        } catch (StringIndexOutOfBoundsException siobe) {
+                            char num = encryption.charAt(encryption.length() - 2);
+                            if (num == 'C') {
+                                char d = encryption.charAt(encryption.length() - 1);
+                                System.out.print(consonants[Character.getNumericValue(d)]);
+                            }
 
                         }
 
                     }
-                    if (FileContent.charAt(i) == 'V') {
-                        if (Character.isDigit(FileContent.charAt(i + 2))) {
-                            String x = FileContent.substring(i + 1, i + 3);
-                            int num = Integer.parseInt(x);
+                    if (c == 'V') {
+                        try {
+                            String Vnum = encryption.substring(i, i + 3);
 
-                            System.out.print(Vowels.get(num));
-                            myWriter.write(Vowels.get(num));
+                            Pattern p = Pattern.compile(regex);
+                            Matcher m = p.matcher(Vnum);
 
-                        } else {
-                            char b = FileContent.charAt(i + 1);
-                            int bnum = Character.getNumericValue(b);
-                            System.out.print(Vowels.get(bnum));
-                            myWriter.write(Vowels.get(bnum));
+                            while (m.find()) {
+                                int num = Integer.parseInt(m.group());
+                                System.out.print(vowels[num]);
+                            }
+                        } catch (StringIndexOutOfBoundsException siobe) {
+                            char num = encryption.charAt(encryption.length() - 2);
+                            if (num == 'V') {
+                                char d = encryption.charAt(encryption.length() - 1);
+                                System.out.print(vowels[Character.getNumericValue(d)]);
+                            }
+
                         }
 
                     }
-                    if (FileContent.charAt(i) == ' ') {
+                    if (c == ' ') {
+                        System.out.print(' ');
 
-                        System.out.print(FileContent.charAt(i));
-                        myWriter.write(FileContent.charAt(i));
                     }
 
                 }
 
             }
-            myReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File doesn't exit");
         }
-        myWriter.close();
-    }
 
+    }
 }
